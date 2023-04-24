46ee42162f42c9db5fb48898fb4a7eccfd79f30a
Osmair <osmair144@gmail.com>
2023-04-24 19:47:16 -0300

    Updated at 20-04-2023

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,36 +9,59 @@ import crepe02 from '../assets/crepe02.jpeg';
 export default function Home() {
 
   return (
-    <main className="flex-col min-h-screen bg-orange-100">
-      <div className='bg-orange-300 flex items-center justify-start flex-wrap h-15 p-2 border-b-2 border-orange-600'>
-        <div><Image src={logo} height={70} width={70} alt='' /></div>
-        <div className="flex-grow font-bold text-center text-red-700 text-4xl font-irish">MENU</div>
-        <div className='flex-col w-25 justify-items-center'>
-          <div className='text-red-700 font-bold'>MESA: 99</div>
-          <div className='text-red-700 font-bold'>PEDIDO: R$ 9,99</div>
+    <main className="flex flex-col justify-between min-h-screen bg-orange-100">
+      <div>
+        <div className='bg-orange-300 flex items-center justify-start flex-wrap h-15 p-2 border-b-2 border-orange-600'>
+          <div><Image src={logo} height={70} width={70} alt='' /></div>
+          <div className="flex-grow font-bold text-center text-red-700 text-4xl font-irish">MENU</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <div className='flex-grow p-2 bg-orange-100 font-bold text-center text-blue-950'>CREPES</div>
+          <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>PRATOS</div>
+          <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>SOBREMESAS</div>
+          <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>BEBIDAS</div>
+        </div>
+
+        <div className='flex flex-col mb-2'>
+          <div className='flex justify-between bg-slate-200 mt-2'>
+            <div className='flex flex-grow justify-start items-center mt-1 pl-2 h-20'>
+              <Image src={crepe01} alt='Crepe' className='w-16 h-16 rounded-full' />
+              <div className='ml-4'>
+                <div className='font-bold text-xm text-left'>Banana + Chocolate</div>
+                <div className='font-bold text-xl text-left'>R$ 9,99</div>
+              </div>
+            </div>
+            <div className='flex flex-col justify-center w-24 mr-4'>
+              <div className='bg-blue-500 p-1 h-8 mb-1 text-center font-semibold'>+ Detalhes</div>
+              <div className='bg-green-500 p-1 h-8 mt-1 text-center font-semibold'>Adicionar</div>
+            </div>
+          </div>
+
+          <div className='flex justify-between bg-slate-200 mt-2'>
+            <div className='flex flex-grow justify-start items-center mt-1 pl-2 h-20'>
+              <Image src={crepe02} alt='Crepe' className='w-16 h-16 rounded-full' />
+              <div className='ml-4'>
+                <div className='font-bold text-xm text-left'>Frango + Azeitona</div>
+                <div className='font-bold text-xl text-left'>R$ 9,99</div>
+              </div>
+            </div>
+            <div className='flex flex-col justify-center w-24 mr-4'>
+              <div className='bg-blue-500 p-1 h-8 mb-1 text-center font-semibold'>+ Detalhes</div>
+              <div className='bg-green-500 p-1 h-8 mt-1 text-center font-semibold'>Adicionar</div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className='flex justify-between'>
-        <div className='flex-grow p-2 bg-orange-100 font-bold text-center text-blue-950'>CREPES</div>
-        <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>PRATOS</div>
-        <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>SOBREMESAS</div>
-        <div className='flex-grow p-2 bg-orange-300 font-bold text-center text-blue-950'>BEBIDAS</div>
-      </div>
-
-      <div className='flex flex-wrap justify-center'>
-        <div className='flex flex-col justify-around items-center mt-10 ml-5 w-64 h-64 bg-slate-200'>
-          <Image src={crepe01} alt='Crepe' className='w-40 h-40 rounded-full' />
-          <div className='font-bold text-xm text-center'>Banana + Chocolate</div>
-          <div className='font-bold text-xl text-center'>R$ 9,99</div>
+      <div className='flex justify-between items-center bg-orange-300 p-2'>
+        <div className='flex flex-col flex-grow'>
+          <div className='font-bold'>Itens: 9</div>
+          <div className='font-bold'>Total: R$ 9,99</div>
         </div>
-
-        <div className='flex flex-col justify-around items-center mt-10 ml-5 w-64 h-64 bg-slate-200'>
-          <Image src={crepe02} alt='Crepe' className='w-40 h-40 rounded-full' />
-          <div className='font-bold text-xm text-center'>Frango + Azeitona</div>
-          <div className='font-bold text-xl text-center'>R$ 9,99</div>
+        <div>
+          <div className='bg-blue-500 w-24 text-center font-bold p-2'>Detalhes Pedido</div>
         </div>
-
       </div>
     </main>
   )
